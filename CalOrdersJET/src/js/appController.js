a01ad79e13bfd0e7ee15f0723caecf77e6bbcfcb
rowens-oncore <royce.owens@oncorellc.com>
2017-02-10 15:13:00 -0800

    [#139504917][#139505609] building navigation and integrating login checkpoint

--- CalOrdersJET/src/js/appController.js
+++ CalOrdersJET/src/js/appController.js
@@ -1,25 +1,16 @@
-/* global Storage */
-
-/**
- * Copyright (c) 2014, 2016, Oracle and/or its affiliates.
- * The Universal Permissive License (UPL), Version 1.0
- */
-
 /* 
- * The OnCore Consulting LLC License
+ * The MIT License
  *
- * Copyright 2016 OnCore Consulting LLC, All Rights Reserved.
+ * Copyright 2017 oncore.
  *
- * Permission IS NOT GRANTED to any person obtaining a copy
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, unless the following conditions are met:
+ * furnished to do so, subject to the following conditions:
  *
- * Written permission is obtained from  
- * OnCore Consulting LLC and the above copyright 
- * notice and this permission notice shall be included in
+ * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
