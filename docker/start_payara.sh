f3dfc65e199b4edb9ed3f2b8ce7ba7a044c9ab1c
Kyle Poland <kyle.poland@oncorellc.com>
2017-02-13 22:54:12 -0800

    move things that assumed root in container

--- docker/start_payara.sh
+++ docker/start_payara.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 /opt/payara41/bin/asadmin start-domain
-/opt/payara41/bin/asadmin -u admin deploy --force=true /CalOrdersRest.war
+/opt/payara41/bin/asadmin -u admin deploy --force=true /opt/payara41/deployments/CalOrdersRest.war
 /bin/bash -c "while true; do sleep 60; done"
