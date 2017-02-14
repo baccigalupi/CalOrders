a30d1ae222c9c0db71b9def8979ca985467cab19
mtsay-oncore <michael.tsay@oncorellc.com>
2017-02-14 10:05:32 -0800

    [#139505609]adding gecko to support firefox

--- CalOrdersSelenium/test/test/oncore/calorders/selenium/EndUserHomePageTest.java
+++ CalOrdersSelenium/test/test/oncore/calorders/selenium/EndUserHomePageTest.java
@@ -1,19 +1,25 @@
 package test.oncore.calorders.selenium;
 
-import com.thoughtworks.selenium.*;
+import com.thoughtworks.selenium.Selenium;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
+import com.thoughtworks.selenium.webdriven.WebDriverBackedSelenium;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import java.util.regex.Pattern;
+import static org.apache.commons.lang3.StringUtils.join;
 
 public class EndUserHomePageTest {
 	private Selenium selenium;
 
 	@Before
 	public void setUp() throws Exception {
-		selenium = new DefaultSelenium("localhost", 4444, "*chrome", "http://localhost:8000/");
-		selenium.start();
+                System.setProperty("webdriver.gecko.driver","/home/oncore/workspaces/CalOrders/libs/selenium-java-3.0.1/geckodriver");
+		WebDriver driver = new FirefoxDriver();
+		String baseUrl = "http://localhost:8000/";
+		selenium = new WebDriverBackedSelenium(driver, baseUrl);
 	}
 
 	@Test
@@ -21,7 +27,6 @@ public class EndUserHomePageTest {
 		selenium.open("/");
 		selenium.click("id=login");
 		selenium.type("id=userName", "rickybobby");
-		selenium.click("css=div.oj-panel.oj-flex");
 		selenium.click("id=continue");
 	}
 
