fe43bc0b231e89ebf63dfe40640bacec8dd9354d
Kyle Poland <kyle.poland@oncorellc.com>
2017-02-15 08:14:44 -0800

    adjust container startup

--- docker/start_payara.sh
+++ docker/start_payara.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-/opt/payara41/bin/asadmin start-domain
-/opt/payara41/bin/asadmin -u admin deploy --force=true /opt/payara41/deployments/CalOrdersRest.war
+/opt/payara41/customize_domainXML.pl /opt/payara41/glassfish/domains/payaradomain/config/domain.xml
+/opt/payara41/bin/asadmin start-domain payaradomain
+/opt/payara41/bin/asadmin -u admin deploy --force=true /CalOrdersRest.war
 /bin/bash -c "while true; do sleep 60; done"
