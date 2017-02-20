e980328b527a5933d73768f3b6134a573e8e794d
rowens-oncore <royce.owens@oncorellc.com>
2017-02-19 21:17:08 -0800

    [#140095601] built service for first chart on admin dashboard

--- CalOrdersRest/src/java/com/oncore/calorders/rest/service/extension/OrderHistoryFacadeRESTExtension.java
+++ CalOrdersRest/src/java/com/oncore/calorders/rest/service/extension/OrderHistoryFacadeRESTExtension.java
@@ -33,22 +33,32 @@ import com.oncore.calorders.rest.OrderHistory;
 import com.oncore.calorders.rest.OrderProductAssoc;
 import com.oncore.calorders.rest.Party;
 import com.oncore.calorders.rest.Product;
+import com.oncore.calorders.rest.data.OrderItemData;
+import com.oncore.calorders.rest.data.OrdersByQuarterData;
+import com.oncore.calorders.rest.data.OrdersByQuarterSeriesData;
 import com.oncore.calorders.rest.service.OrdStatusCdFacadeREST;
 import com.oncore.calorders.rest.service.OrderHistoryFacadeREST;
 import java.io.StringReader;
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.apache.commons.collections4.CollectionUtils;
 
 /**
  *
@@ -158,6 +168,166 @@ public class OrderHistoryFacadeRESTExtension extends OrderHistoryFacadeREST {
             throw new DataAccessException(ex, ErrorCode.DATAACCESSERROR);
         }
 
+    }
+
+    /**
+     * Find all orders grouped by quarter for the last 4 years. For this
+     * iteration the orders are pulled for the last four years (including the
+     * current year), which are 2017,16,15,14
+     *
+     * @param ptyUid a valid party uid
+     *
+     * @return a structure of order totals grouped by quarter
+     *
+     * @throws com.oncore.calorders.core.exceptions.DataAccessException
+     */
+    @GET
+    @Path("fetchOrdersByQuarter/{ptyUid}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public OrdersByQuarterSeriesData fetchOrdersByQuarter(@PathParam("ptyUid") Integer ptyUid) throws DataAccessException {
+
+        List<OrderHistory> orderHistoryList = null;
+        OrdersByQuarterSeriesData ordersByQuarterSeriesData = new OrdersByQuarterSeriesData();
+        OrdersByQuarterData ordersByQuarterData = null;
+        OrderItemData orderItemData = null;
+        Calendar cal = Calendar.getInstance();
+
+        ordersByQuarterData = new OrdersByQuarterData();
+        ordersByQuarterData.setName("Jan");
+        ordersByQuarterSeriesData.getOrdersByQuarterDataList().add(ordersByQuarterData);
+
+        ordersByQuarterData = new OrdersByQuarterData();
+        ordersByQuarterData.setName("Apr");
+        ordersByQuarterSeriesData.getOrdersByQuarterDataList().add(ordersByQuarterData);
+
+        ordersByQuarterData = new OrdersByQuarterData();
+        ordersByQuarterData.setName("Jul");
+        ordersByQuarterSeriesData.getOrdersByQuarterDataList().add(ordersByQuarterData);
+
+        ordersByQuarterData = new OrdersByQuarterData();
+        ordersByQuarterData.setName("Oct");
+        ordersByQuarterSeriesData.getOrdersByQuarterDataList().add(ordersByQuarterData);
+
+        try {
+
+            Logger.debug(LOG, "Hey testing logging, the fetchOrdersByQuarter is being called!");
+
+            Party party = getEntityManager().find(Party.class, ptyUid);
+
+            orderHistoryList = getEntityManager().createQuery("SELECT o FROM OrderHistory o WHERE o.ptyUidFk = :ptyUidFk AND o.createTs > '2014:01:01 15:06:39.673' ORDER BY o.createTs ASC", OrderHistory.class).setParameter("ptyUidFk", party).getResultList();
+
+            String month = null;
+            Integer year = null;
+
+            if (CollectionUtils.isNotEmpty(orderHistoryList)) {
+
+                for (OrderHistory order : orderHistoryList) {
+                    cal.setTime(order.getCreateTs());
+
+                    month = this.getQuarterMonth(cal.get(Calendar.MONTH));
+                    year = cal.get(Calendar.YEAR);
+
+                    if (year.equals(2015)) {
+                        year = 2015;
+                    }
+
+                    boolean found = false;
+
+                    for (OrdersByQuarterData quarter : ordersByQuarterSeriesData.getOrdersByQuarterDataList()) {
+                        if (month.equalsIgnoreCase(quarter.getName())) {
+
+                            found = false;
+
+                            if (CollectionUtils.isEmpty(quarter.getOrderItemDataList())) {
+                                OrderItemData item = new OrderItemData();
+                                item.setYear(year);
+                                item.setY(1);
+                                item.setLabel(1);
+                                quarter.getOrderItemDataList().add(item);
+                            } else {
+                                for (OrderItemData item : quarter.getOrderItemDataList()) {
+                                    if (year.equals(item.getYear())) {
+                                        item.setY(item.getY() + 1);
+                                        item.setLabel(item.getY());
+                                        found = true;
+                                        break;
+                                    }
+
+                                }
+
+                                if (!found) {
+                                    OrderItemData item = new OrderItemData();
+                                    item.setYear(year);
+                                    item.setY(1);
+                                    item.setLabel(1);
+                                    quarter.getOrderItemDataList().add(item);
+                                    break;
+                                }
+
+                            }
+
+                        }
+                    }
+
+                }
+
+            }
+
+        } catch (Exception ex) {
+            Logger.error(LOG, FormatHelper.getStackTrace(ex));
+            throw new DataAccessException(ex, ErrorCode.DATAACCESSERROR);
+        }
+
+        return ordersByQuarterSeriesData;
+
+    }
+
+    private String getQuarterMonth(int month) {
+        String result = "Jan";
+
+        switch (month) {
+            case 1:
+                result = "Jan";
+                break;
+            case 2:
+                result = "Jan";
+                break;
+            case 3:
+                result = "Jan";
+                break;
+            case 4:
+                result = "Apr";
+                break;
+            case 5:
+                result = "Apr";
+                break;
+            case 6:
+                result = "Apr";
+                break;
+            case 7:
+                result = "Jul";
+                break;
+            case 8:
+                result = "Jul";
+                break;
+            case 9:
+                result = "Jul";
+                break;
+            case 10:
+                result = "Oct";
+                break;
+            case 11:
+                result = "Oct";
+                break;
+            case 12:
+                result = "Oct";
+                break;
+            default:
+                result = "Jan";
+
+        }
+
+        return result;
     }
 
 }
