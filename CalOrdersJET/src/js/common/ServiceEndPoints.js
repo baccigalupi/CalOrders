63864c8f2feeec69b635093a3228966963252767
oncore <kevin.babcock@oncorellc.com>
2017-02-23 10:45:13 -0800

    [#139517893] - Initial checkin for Order Detail

--- CalOrdersJET/src/js/common/ServiceEndPoints.js
+++ CalOrdersJET/src/js/common/ServiceEndPoints.js
@@ -109,6 +109,12 @@ ServiceEndPoints.prototype = {
         } else if (key === "findAllOrderHistoryByPartyUid")
         {
             return calOrdersHost + "/CalOrdersRest/webresources/com.oncore.calorders.rest.orderHistory/findAllOrderHistoryByPartyUid";
+        } else if (key === "findOrderDetailById")
+        {
+            return calOrdersHost + "/CalOrdersRest/webresources/com.oncore.calorders.rest.orderHistory/findOrderDetailById";
+        } else if (key === "cancelOrderHistory")
+        {
+            return calOrdersHost + "/CalOrdersRest/webresources/com.oncore.calorders.rest.orderHistory/cancelOrderDetail";
         } else
         {
             return "UNDEFINED";
